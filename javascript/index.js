f6c929f3d63f9537925e96899e950c43f0998574
Jorge Eduardo Jiménez Ramírez <Balmund@Thaumazo.lan>
2024-07-19 18:39:17 -0400

    Promises were fulfilled.

--- javascript/index.js
+++ javascript/index.js
@@ -34,38 +34,43 @@
 
 obtainInstruction('steak', 0)
 
-.then( (step0) => { document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
-return obtainInstruction('steak', 1); })
+.then( (step0) => {
+  document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+  return obtainInstruction('steak', 1); })
 
-.then( (step1) => { document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
- return obtainInstruction('steak', 2); })
+.then( (step1) => { 
+  document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+  return obtainInstruction('steak', 2); })
 
-.then( (step2) => { document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
- return obtainInstruction('steak', 3); })
+.then( (step2) => { 
+  document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+  return obtainInstruction('steak', 3); })
 
- .then( (step3) => { document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+ .then( (step3) => { 
+  document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
   return obtainInstruction('steak', 4); })
 
- .then( (step4) => { document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+ .then( (step4) => {
+  document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
   return obtainInstruction('steak', 5); })
 
- .then( (step5) => { document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+ .then( (step5) => {
+  document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
   return obtainInstruction('steak', 6); })
 
- .then( (step6) => { document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+ .then( (step6) => {
+  document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
   return obtainInstruction('steak', 7);})
 
- .then( (step7) => { document.querySelector("#steak").innerHTML += `<li>${"The steak is ready! "}</li>`})
-.catch((errorMsg) => console.log(errorMsg))
+ .then( (step7) => {
+  document.querySelector("#steak").innerHTML += `<li>${"The steak is ready! "}</li>`})
+
+  .catch((errorMsg) => console.log(errorMsg))
 
 // Iteration 3 using async/await
 // ...
 
 // Bonus 2 - Promise all
-// ...
-
-//this is a comment to initialized the work
-//testing submission and stuff
 
 const instruction1 = new Promise((resolve) => setTimeout(resolve, 500, 'wash brussels sprouts'));
 const instruction2 = new Promise((resolve) => setTimeout(resolve, 500, 'cut off base and chop in half'));
