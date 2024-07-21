8fbb35056a61a2d94825a851fc8c6dd31b31e731
Jorge Eduardo Jiménez Ramírez <Balmund@Thaumazo.lan>
2024-07-21 14:58:01 -0400

    Latest changes

--- javascript/index.js
+++ javascript/index.js
@@ -10,7 +10,7 @@ getInstruction("mashedPotatoes", 0, (step1) => {
         document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
         getInstruction("mashedPotatoes", 4, (step5) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-          document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!<li>`;
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
           document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
         }, (error) => console.log(error));
       }, (error) => console.log(error));
